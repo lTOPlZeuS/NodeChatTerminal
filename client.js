b26f619a7662ce017a1fd81b1f248ab4f4d29bae
Jesus A Rocha V <jesus_reddead@hotmail.com>
2021-04-26 22:29:52 -0500

    Primer commit

---
+++ client.js
@@ -0,0 +1,15 @@
+const { Socket } = require("net");
+const readline = require("readline").createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+const socket = new Socket();
+
+socket.connect({ host: "localhost", port: 8000 });
+socket.setEncoding("utf-8");
+readline.on("line", (line) => {
+  socket.write(line);
+});
+socket.on("data", (data) => {
+  console.log(data);
+});
